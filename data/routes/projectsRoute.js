f64a28e1519d8bf9c6f757d83c57ffb16be7881b
Mike Landers <landers.mike@gmail.com>
2019-02-15 09:16:35 -0800

    Update to GET to grab the id from the url query

--- data/routes/projectsRoute.js
+++ data/routes/projectsRoute.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
 		res.status(201).json({ success: true, project });
 	} catch {
 		res.status(500).json({
-			error: 'The posts information could not be retrieved.',
+			error: 'The project information could not be saved.',
 		});
 	}
 });
@@ -19,11 +19,12 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
 	try {
-		const projects = await projectsdb.get();
+		const { id } = req.query;
+		const projects = await projectsdb.get(id);
 		res.status(200).json({ success: true, projects });
 	} catch (error) {
 		res.status(500).json({
-			error: 'The posts information could not be retrieved.',
+			error: 'The project information could not be retrieved.',
 		});
 	}
 });
@@ -37,13 +38,40 @@ router.get('/:id', async (req, res) => {
 		res.status(200).json({ success: true, projectActions });
 	} catch (error) {
 		res.status(500).json({
-			error: 'The posts information could not be retrieved.',
+			error: 'The project information could not be retrieved.',
 		});
 	}
 });
 
 //UPDATE
 
+router.put('/:id', async (req, res) => {
+	try {
+		const { name, description, completed } = req.body;
+		const { id } = req.params;
+		const changes = { name, description, completed };
+		const updatedProject = await projectsdb.update(id, changes);
+		res.status(200).json({ success: true, updatedProject });
+	} catch (error) {
+		res.status(500).json({
+			error: 'The project information could not be updated.',
+		});
+	}
+});
+
 //DESTROY
+
+router.delete('/:id', async (res, req) => {
+	try {
+		const { id } = req.params;
+		const deleted = await projectsdb.remove(id);
+
+		res.status(200).json({ success: true, message: 'Successfully deleted the project' });
+	} catch (error) {
+		res.status(500).json({
+			error: 'The project could not be deleted.',
+		});
+	}
+});
 
 module.exports = router;
