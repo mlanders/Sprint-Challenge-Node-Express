53f239af0431c1b58d3011d59d0cd0ec10633036
Mike Landers <landers.mike@gmail.com>
2019-02-15 10:46:45 -0800

    final updates

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
 
